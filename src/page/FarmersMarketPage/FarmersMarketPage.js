2b3c51f6eb47aff3d656aa13a25d8d4fcbe2dad6
Kris Zernikel <kriszernikel@Kriss-MacBook-Pro.local>
2021-12-29 03:03:26 -0600

    Beggining of plugging in farmers markets api

---
+++ src/page/FarmersMarketPage/FarmersMarketPage.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+async function getMarketsByZip (zip) {
+  const res = await axios.get('http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' + zip)
+
+  console.log(res.data)
+
+  return res.data
+}
+
+export function FarmersMarketPage () {
+  const [data, setData] = useState(null)
+
+  async function handleChange (event) {
+    const zip = event.target.value
+
+    if (zip >= 10000 && zip <= 99999) {
+      const data = await getMarketsByZip(zip)
+
+      setData(data)
+    }
+  }
+
+  return (
+    <>
+      <h1>Farmers Markets</h1>
+      <p>Find farmers markets</p>
+      {((data && data.results) || []).map((market, index) => {
+        return <li key={index}>{JSON.stringify(market)}</li>
+      })}
+      <br />
+      <input type='number' onChange={handleChange} />
+    </>
+  )
+}
--- src/page/index.js
+++ src/page/index.js
@@ -1,4 +1,5 @@
 export * from './AboutUsPage'
+export * from './FarmersMarketPage'
 export * from './HomePage'
 export * from './NotFoundPage'
 export * from './PartnersPage'
