4b52147ba4811124da7e7be5d54c7c7736e99578
Kris Zernikel <kriszernikel@Kriss-MacBook-Pro.local>
2021-12-30 04:18:44 -0600

    Caching farmersMarketsApi

--- src/service/UsdaService.js
+++ src/service/UsdaService.js
@@ -3,10 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const farmersMarketsApi = createApi({
   reducerPath: 'farmersMarketsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://search.ams.usda.gov/' }),
   endpoints: (builder) => ({
     getFarmersMarketsByZipCode: builder.query({
-      query: (zipCode) => `zipSearch?zip=${zipCode}`
+      query: (zipCode) => `farmersmarkets/v1/data.svc/zipSearch?zip=${zipCode}`
     })
   })
 })
